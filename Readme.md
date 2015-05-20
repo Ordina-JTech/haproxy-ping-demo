c54188eaa9d50bf09a366b7450aebbce2caa1069
Teije van Sloten <teije.van.sloten@hva.nl>
2015-05-20 17:15:47 +0200

    Some style changes. Updated read-me.

--- Readme.md
+++ Readme.md
@@ -3,13 +3,14 @@
 This project contains 3 docker containers, two apache servers and one haproxy server which load balance between these two servers.
 More information about this project can be found in this blog: http://robbertvdzon.blogspot.nl/2015/03/using-haproxy-for-loadbalancing-2-web.html
 
-To build and run all three containers, you can use the build.sh command.
+To build and run all three containers, you can use the `build.sh` command.
 After the containers are started, the following addresses are available:
 
-http://[ip]:80     demo website
-
-http://[ip]:81     haproxy admin site
-
-http://[ip]:84     haproxy admin site
-
-http://[ip]:85     haproxy admin site
+* http://[ip]:80     
+..* demo website
+* http://[ip]:81     
+..* haproxy admin site
+* http://[ip]:84     
+..* haproxy admin site
+* http://[ip]:85     
+..* haproxy admin site
--- apache/app/index.php
+++ apache/app/index.php
@@ -1,94 +1,79 @@
 
 <!doctype html>
 <html >
-<head>
+    <head>
+        <title>HaProxy-Ping-Demo | Ordina</title>
+        <style media="screen" type="text/css">
+            body {
+                padding: 0;
+                margin: 0;
+            }
 
-<style media="screen" type="text/css">
-td, th, h3, body
-{
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	color: #444444;
-	font-size: 40px;
+            table {
+                border-spacing: 0px 10px;
+                width: 100%;
+                border: 0;
+            }
+            
+            td, th, h3, body {
+                font-family: Verdana, Arial, Helvetica, sans-serif;
+                color: #444444;
+                font-size: 40px;
+            }
+            
+            img {
+                height: 200px;
+            }
 
-}
+            h3 {
+                font-size: 50px;
+                color: #FFFFFF;
+            }
 
-table{
-    border-spacing: 0px 10px;
-}
+            .header {
+                background-color: #e88200;
+                padding-top: 0px;
+                padding-bottom: 0px;
+            }
 
+            .headerTitle {
+                padding: 1px 1px 0 1px;
+                text-align: center;
+            }
+        </style>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    </head>
+    <body >
 
-h3
-{
-	font-size: 50px;
-	color: #FFFFFF;
-}
-
-
-.header {
-    background-color: #e88200;
-    layer-background-color: #e88200;
-    padding-top: 0px;
-    padding-bottom: 0px;
-}
-
-
-
-.headerTitle {
-    padding-top: 1px;
-    padding-left: 1px;
-    padding-right: 1px;
-    text-align: center;
-}
-
-
-body {
-  padding-top: 0px;
-  padding-bottom: 0px;
-  margin-top: 0px;
-  margin-bottom: 0px;
-}
-
-</style>
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-</script>
-
-<script>
-
- $(document).ready(function(){
- 	setInterval(function(){cache_clear()},1000);
- });
-
- function cache_clear(){
-    window.location.reload(true);
- }
-</script>
-
-</head>
-<body >
-
-<div class="container" >
-    <div class="header">
-        <div class="headerTitle">
-        	<table width=100% border=0>
-        	<tr>
-        	<td width=20>
-        	</td>
-        	<td width=320>
-			<img src="http://www.mmwb.nl/wp-content/uploads/2014/10/ordina-logo-455x300.jpg?81fc35" height=200/>
-        	</td>
-        	<td>
-            <h3>Sample webpage</h3>
-        	</td>
-        	</tr>
-        	</table>
+        <div>
+            <div class="header">
+                <div class="headerTitle">
+                    <table>
+                        <tr>
+                            <td style="width: 20px;"></td>
+                            <td style="width: 320px;">
+                                <img src="http://www.mmwb.nl/wp-content/uploads/2014/10/ordina-logo-455x300.jpg?81fc35" alt="ordina" />
+                            </td>
+                            <td>
+                                <h3>Sample web-page</h3>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <br>
+            Sample web-page hosted on server <?php echo $_SERVER['SERVER_ADDR']; ?>
         </div>
-    </div>
-	<br>
-	Sample webpage hosted on server <?php echo $_SERVER['SERVER_ADDR']; ?>
+        <script>
+            $(document).ready(function () {
+                setInterval(function () {
+                    cache_clear();
+                }, 1000);
+            });
 
-</div>
-
-
-</body>
+            function cache_clear() {
+                window.location.reload(true);
+            }
+        </script>
+    </body>
 </html>
